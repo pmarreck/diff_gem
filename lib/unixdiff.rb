13b1d335f4a73ade3099ffdcb975ad172cd44d47
Peter Marreck <pmarreck@peter.internal.salesforce.com>
2012-04-19 17:44:34 -0700

    Changed to make to_diff return a string

--- lib/unixdiff.rb
+++ lib/unixdiff.rb
@@ -1,62 +1,57 @@
 #!/usr/bin/env ruby
 
-require 'diff'
-
-def loadfile(filename)
-  lines = nil
-  File.open(filename, "r") { |f|
-    lines = f.readlines
-  }
-  return lines
-end
-
-def diffrange(a, b)
-  if (a == b)
-    "#{a}"
-  else
-    "#{a},#{b}"
-  end
-end
+require_relative 'diff'
 
 class Diff
-  def to_diff(io = $defout)
+  def to_diff
+    out = ''
     offset = 0
-    @diffs.each { |b|
+    @diffs.each do |b|
       first = b[0][1]
       length = b.length
       action = b[0][0]
       addcount = 0
       remcount = 0
-      b.each { |l| 
+      b.each do |l|
         if l[0] == "+"
           addcount += 1
         elsif l[0] == "-"
           remcount += 1
         end
-      }
+      end
       if addcount == 0
-        puts "#{diffrange(first+1, first+remcount)}d#{first+offset}"
+        out << "#{diffrange(first+1, first+remcount)}d#{first+offset}\n"
       elsif remcount == 0
-        puts "#{first-offset}a#{diffrange(first+1, first+addcount)}"
+        out << "#{first-offset}a#{diffrange(first+1, first+addcount)}\n"
       else
-        puts "#{diffrange(first+1, first+remcount)}c#{diffrange(first+offset+1, first+offset+addcount)}"
+        out << "#{diffrange(first+1, first+remcount)}c#{diffrange(first+offset+1, first+offset+addcount)}\n"
       end
       lastdel = (b[0][0] == "-")
-      b.each { |l|
+      b.each do |l|
         if l[0] == "-"
           offset -= 1
-          print "< "
+          out << "< "
         elsif l[0] == "+"
           offset += 1
-	  if lastdel
-	    lastdel = false
-            puts "---"
+          if lastdel
+            lastdel = false
+            out << "---\n"
           end
-          print "> "
+          out << "> "
         end
-	print l[2]
-      }
-    }
+        out << l[2]
+      end
+    end
+    out
+  end
+
+  private
+  def diffrange(a, b)
+    if (a == b)
+      "#{a}"
+    else
+      "#{a},#{b}"
+    end
   end
 end
 
@@ -65,10 +60,10 @@ if $0 == __FILE__
   file1 = ARGV.shift
   file2 = ARGV.shift
 
-  ary1 = loadfile file1
-  ary2 = loadfile file2
+  ary1 = File.read file1
+  ary2 = File.read file2
 
   diff = Diff.new(ary1, ary2)
-  diff.to_diff
+  puts diff.to_diff
 
 end
